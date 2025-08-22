e83b6e84f22f89b86317cb154dcf297120ec2445
itsuki <itsuki.enjyoy@gmail.com>
2025-08-23 08:56:48 +0900

    Initial Commit

---
+++ AudioRecordingDemo/AudioRecordingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecordingDemoApp.swift
+//  AudioRecordingDemo
+//
+//  Created by Itsuki on 2025/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecordingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
